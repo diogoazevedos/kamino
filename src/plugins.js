aef372ceb082a4f44d8cc65df82438b3ff4f473d
Diogo Azevedo <diogoazevedos@gmail.com>
2017-02-09 18:38:28 -0200

    refactor: Remove extra folder level

---
+++ src/plugins.js
@@ -0,0 +1,16 @@
+/* eslint-disable global-require */
+
+module.exports = [
+  {
+    register: require('./product'),
+    routes: {
+      prefix: '/products',
+    },
+  },
+  {
+    register: require('./user'),
+    routes: {
+      prefix: '/users',
+    },
+  },
+];
