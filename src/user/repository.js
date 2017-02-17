99d72bb57435272ab932f43bd3014cc108372de7
Diogo Azevedo <diogoazevedos@gmail.com>
2017-02-16 23:01:17 -0200

    refactor: Update create to use find method

--- src/user/repository.js
+++ src/user/repository.js
@@ -1,12 +1,27 @@
-const { merge } = require('ramda');
+const { is, not, head, curry, compose } = require('ramda');
 const User = require('./user');
 
-exports.create = (knex, data) => (
-  knex('users')
-    .insert(data)
-    .then((result) => {
-      const attributes = merge(data, { id: result[0] });
+const find = curry((knex, id) => {
+  if (not(is(Number, id))) {
+    throw new TypeError('The id must be a number');
+  }
 
-      return User.of(attributes);
-    })
-);
+  return knex('users')
+    .where({ id })
+    .then(compose(User.of, head));
+});
+
+const create = curry((knex, user) => {
+  if (not(is(User, user))) {
+    throw new TypeError('The user must be an user');
+  }
+
+  return knex('users')
+    .insert(user.props)
+    .then(compose(find(knex), head));
+});
+
+module.exports = {
+  find,
+  create,
+};
