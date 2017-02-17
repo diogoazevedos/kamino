785960d458c5b6eb43db06048b6f212ad351b7bb
Diogo Azevedo <diogoazevedos@gmail.com>
2017-02-16 22:50:25 -0200

    feat: Add user getters and setters

--- src/user/user.js
+++ src/user/user.js
@@ -1,10 +1,41 @@
+const { gt, is, not, length } = require('ramda');
+
 class User {
-  constructor(attributes) {
-    this.attributes = attributes;
+  constructor(props) {
+    this.props = { };
+    Object.assign(this, props);
   }
 
-  static of(attributes) {
-    return new User(attributes);
+  set id(value) {
+    if (not(is(Number, value))) {
+      throw new TypeError('The id must be a number');
+    }
+
+    this.props.id = value;
+  }
+
+  get id() {
+    return this.props.id;
+  }
+
+  set name(value) {
+    if (not(is(String, value))) {
+      throw new TypeError('The name must be a string');
+    }
+
+    if (gt(length(value), 255)) {
+      throw new RangeError('The name may not be greater than 255 caracterers');
+    }
+
+    this.props.name = value;
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  static of(props) {
+    return new User(props);
   }
 }
 
